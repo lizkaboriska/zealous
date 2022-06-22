ab6c5072a4b964d2ebd67f152381d9af75426195
Elizaveta Borisova <lizkaboriska@gmail.com>
2022-06-22 23:15:42 +0200

    Console warning fix: ReactDOM.render -> ReactDOM.createRoot

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -1,19 +1,22 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from '@zealous/ui'
 
 import App from './App'
 import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 import reportWebVitals from './reportWebVitals'
 
-ReactDOM.render(
+const rootElement = document.getElementById('root')
+if (!rootElement) throw new Error('Failed to find the root element')
+
+const root = ReactDOM.createRoot(rootElement)
+
+root.render(
   <React.StrictMode>
     <ThemeProvider>
       <App />
     </ThemeProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+  </React.StrictMode>)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
