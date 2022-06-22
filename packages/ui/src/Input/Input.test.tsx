e36f87eabc36772aee78b96c0ab6682a44d13032
Elizaveta Borisova <lizkaboriska@gmail.com>
2022-06-22 23:13:58 +0200

    Add tests for email input

--- packages/ui/src/Input/Input.test.tsx
+++ packages/ui/src/Input/Input.test.tsx
@@ -2,12 +2,14 @@ import { render, screen, act } from '@testing-library/react'
 import { composeStories } from '@storybook/testing-react'
 import { StoryContext } from '@storybook/react'
 
-import { INPUT_SELECTOR } from './Input'
+import { INPUT_ERR, INPUT_SELECTOR } from './Input'
 import * as stories from './Input.story'
+import { userEvent } from '@storybook/testing-library'
 
 const {
   Default,
-  Controlled
+  Controlled,
+  Email
 } = composeStories(stories)
 
 it('should be possible to use a text input', async () => {
@@ -29,4 +31,40 @@ it('should be possible to control a text input', async () => {
   expect(
     onChange
   ).toHaveBeenCalledTimes('hello world'.length)
+})
+
+it('should be possible to use email input', async () => {
+  render(<Email />)
+
+  expect(
+    await screen.findByTestId(INPUT_SELECTOR)
+  ).toBeInTheDocument()
+})
+
+it('should show error when email input is incorrect', async () => {
+  const { findByTestId, queryByTestId } = render(<Email />)
+  let err: HTMLElement = null
+
+  await act(async () => {
+    userEvent.type(await findByTestId(INPUT_SELECTOR), 'incorrect email.com')
+    err = queryByTestId(INPUT_ERR)
+  })
+
+  expect(
+    err
+  ).not.toBeNull()
+})
+
+it('should not show error when email input is correct', async () => {
+  const { findByTestId, queryByTestId } = render(<Email />)
+  let err: HTMLElement = null
+
+  await act(async () => {
+    userEvent.type(await findByTestId(INPUT_SELECTOR), 'correct@email.com')
+    err = queryByTestId(INPUT_ERR)
+  })
+
+  expect(
+    err
+  ).toBeNull()
 })
